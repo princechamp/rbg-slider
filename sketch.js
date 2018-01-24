4277a9d25e5914c462dfa17072af57b4491072fd
Prince Onwuvuche <ponwuvuche@wlapcs.org>
2018-01-24 03:33:16 +0000

    fixed everything

--- sketch.js
+++ sketch.js
@@ -4,15 +4,22 @@ var joeSlider;
 
 function setup(){
     createCanvas(600,400);
-    background(0,2,1);
+    background(0);
     
     //createSlider(min,max,default)
     princeSlider = createSlider(0,255,0);
     princeSlider.position(20,20);
     
+    joeSlider = createSlider(0,255,0);
+    joeSlider.position(20,50);
+    
+    craigSlider = createSlider(0,255,0);
+    craigSlider.position(20,80);
 }
 
 function draw(){
-     redValue = princeSlider.value();
-     background(redValue,0,0);
+    redValue = princeSlider.value();
+    greenValue = joeSlider.value();
+    blueValue = craigSlider.value();
+    background(redValue, greenValue, blueValue);
 }
